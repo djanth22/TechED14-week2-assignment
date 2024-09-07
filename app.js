7720d4e0b83e4fa8982a5a7ba919b99c0c639e16
David Anthony <davywavy22@googlemail.com>
2024-09-07 15:57:11 +0100

    got thumbnails and main images working, including previous and next buttons, just need to get the thumbnail button and the events for the thumbnail images to work

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ console.log("log test, hello there");
 const displayImage = document.getElementById(`display-image`);
 let thumbnailContainer = document.getElementById(`Thumbnail-container`);
 
+// buttons
+const prev = document.getElementById(`previous`);
+const next = document.getElementById(`next`);
+
 // for later
 let currentImageIndex = 0;
 
@@ -54,5 +58,32 @@ function createThumbnails() {
   }
 }
 // // testing function
-// createThumbnails();
+createThumbnails();
+updateCurrentImage(images[currentImageIndex]);
 // it worked
+
+// main image
+function updateCurrentImage() {
+  displayImage.innerHTML = "";
+  let img = document.createElement("img");
+  img.setAttribute(`src`, images[currentImageIndex].src);
+  img.setAttribute(`alt`, images[currentImageIndex].alt);
+  img.classList.add(`displayed-image`);
+  displayImage.appendChild(img);
+}
+
+// buttons
+// I want a function here that updates the image index as an event handler for the buttons
+function updateIndex(index) {
+  currentImageIndex += index;
+  if (currentImageIndex >= images.length) currentImageIndex = 0;
+  if (currentImageIndex < 0) currentImageIndex = images.length - 1;
+  updateCurrentImage(images[currentImageIndex]);
+}
+
+next.addEventListener("click", function () {
+  updateIndex(1);
+});
+prev.addEventListener("click", function () {
+  updateIndex(-1);
+});
